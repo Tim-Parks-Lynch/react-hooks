e25491f0f9e3c3a5a288feb18a8ada08b34efc19
Sey <76843977+iseykim@users.noreply.github.com>
2021-12-10 13:57:34 -0500

    added demo of counter

---
+++ 01/hooks/src/Counters.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+
+export const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  )
+}
+
+export const CounterEffect = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('CounterEffect running!')
+  }, [])
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          console.log('onClick, count:', count)
+          setCount(count + 1)
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  )
+}
--- 01/hooks/src/main.jsx
+++ 01/hooks/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { Counter, CounterEffect } from './Counters.jsx'
+import 'virtual:windi.css'
+
+export default function Main({ children }) {
+  return (
+    <div>
+      <h1 className="text-center text-4xl mb-8">Main Component</h1>
+      <div className="p-4 border border-pink-200 max-w-400px mx-auto">
+        {children}
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main>
+      <div className="border border-green-300 flex flex-col justify-between h-400px">
+        <Counter />
+        <CounterEffect />
+      </div>
+    </Main>
   </React.StrictMode>,
   document.getElementById('root')
 )
