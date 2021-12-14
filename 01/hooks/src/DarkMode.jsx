35abc0eb1adc2c4afcd6dcd12c6adb33300bf26d
danny-zero <sifuarrow@gmail.com>
2021-12-14 18:30:28 -0500

    added comments to dark-mode hook

--- 01/hooks/src/DarkMode.jsx
+++ 01/hooks/src/DarkMode.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 export default function useDarkMode() {
-  // const [theme, setTheme] = useState(false)
+  //useState can take in a function and that function will be called when React looks for a default value for this useState hook so when the component mounts it will look to see if we have a theme set to dark, if not it will set a localStorage item "theme" to light
   const [theme, setTheme] = useState(() => localStorage.getItem("theme"));
 
   useEffect(() => {
